42628940fcee7305a179ef6213cd2f9e3de1ba44
TheZoq2 <frans.skarman@gmail.com>
2019-01-14 18:54:22 +0100

    Add pwm to rust version of blind opener

--- shadeopener/blindopener/src/main.rs
+++ shadeopener/blindopener/src/main.rs
@@ -1,3 +1,12 @@
+use sysfs_pwm::Pwm;
+
 fn main() {
-    println!("Hello, world!");
+    let pwm = Pwm::new(0, 0).expect("Failed to create pwm");
+
+    pwm.with_exported(|| {
+        pwm.enable(true).expect("Failed to enable pwm");
+        pwm.set_period_ns(20_000_000).expect("Failed to set period");
+        pwm.set_duty_cycle_ns(1_000_000).expect("Failed to set ds");
+        loop{}
+    }).unwrap();
 }
